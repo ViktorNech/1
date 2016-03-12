60c0af8dd80fd26f90fdbc86e0b30d0ec780f8d9
rus0nix <rus.onix@mail.ru>
2016-03-12 14:14:50 +0000

    OK

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
   status = '200 OK'
   response_headers = [('Content-Type','text/plain')]
+  resp = environ['QUERY_STRING'].split("&")
   start_response(status, response_headers)
-  resp = environ['QUERY_STRING']
-  resp = resp.replace('&','\r\n')
+  resp = [item+"\r\n" for item in resp]
   return resp
