b3112ece1a766f52174b83cd577d8007b8010838
rus0nix <rus.onix@mail.ru>
2016-03-12 10:10:18 +0000

    Gunicorn2

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/test
+sudo rm -rf /etc/gunicorn.d/wsgi.example
+sudo /etc/init.d/gunicorn restart
