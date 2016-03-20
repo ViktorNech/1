1d2e0d1637684d3b0f519a3b7170db74bd366cbd
Onix <rus.onix@mail.ru>
2016-03-20 19:07:33 +0300

    OK

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
+    'ask',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qa',
+        'USER': 'qa',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,3 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+# Create your views here.
 def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+    return HttpResponse('OK', status=200)
